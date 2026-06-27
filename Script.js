b6f3f887796d1072adf7ba4a9ebb89c733b1ddfa
harjot-oss <harjotsingh241007@gmail.com>
2026-06-27 09:33:28 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,21 @@
+const topBtn = document.getElementById("topBtn");
+
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 200) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+};
+
+topBtn.onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+};
+
+document.getElementById("contactForm").addEventListener("submit", function(e){
+    e.preventDefault();
+    alert("Message Sent Successfully!");
+});
